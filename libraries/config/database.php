711991708d20c8e56d83d5ebd00980e004f2010b
MathieuElisabeth <mathieuregina75@gmail.com>
2022-12-26 11:48:34 +0100

    test

--- libraries/config/database.php
+++ libraries/config/database.php
@@ -1,6 +1,6 @@
 <?php
 require '../../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__, 3));
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__, 4));
 $dotenv->safeLoad(); 
 
 /**
